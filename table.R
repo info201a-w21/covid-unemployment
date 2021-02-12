bf7e73a254ad5617ffaa56939460bb2d846432ac
annashi127 <ashi127@uw.edu>
2021-02-12 01:52:28 -0800

    Created table file

---
+++ table.R
@@ -0,0 +1,6 @@
+
+library(tidyverse)
+library(knitr)
+
+unemployment <- read.csv("https://raw.githubusercontent.com/info201a-w21/projec
+                         t-JonathanQTrinh/main/Georgetown-by-race.csv")
