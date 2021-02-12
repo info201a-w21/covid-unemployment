b2a77fa85c01209e8c1147e964cb24ef203006fb
Tammy Hu <tammyhu@uw.edu>
2021-02-12 00:44:03 -0800

    new changes

--- chart1.R
+++ chart1.R
@@ -3,8 +3,8 @@ library(styler)
 library(tidyverse)
 library(ggplot2)
 
-unemployment <- read.csv("C:\\Users\\goodj\\Desktop\\Georgetown-by-race.csv",
-                         header = TRUE)
+unemployment <- read.csv("https://raw.githubusercontent.com/info201a-w21/projec
+                         t-JonathanQTrinh/main/Georgetown-by-race.csv")
 
 total_sample_size <- sum(unemployment$Sample.Size)
 
@@ -14,7 +14,7 @@ mod_table <- unemployment %>%
   filter(ï..Labor.Force.Status == "Unemployed") %>% 
   group_by(Race) %>% 
   summarize(num_unemployed = sum(Sample.Size, na.rm = T) / 
-            total_sample_size * 100)
+              total_sample_size * 100)
 
 # graph comparing unemployment percentages by race
 unemployment_by_race <- ggplot(data = mod_table) +
@@ -25,4 +25,5 @@ unemployment_by_race <- ggplot(data = mod_table) +
        title = "Comparing Unemployment Percentages by Race in 2020") +
   scale_fill_manual(values = c("#ff9f1c", "#2ec4b6", "#E65F5C", "#f88dad")) +
   scale_color_manual(values = c("#ff9f1c", "#2ec4b6", "#E65F5C", "#f88dad"))
+
 
