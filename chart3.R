647c43065782caaf93cd243ac10d83223e9eb751
Lynn Chou <lynnchou8@gmail.com>
2021-02-13 14:06:12 -0800

    unemployment dataframe complete

--- chart3.R
+++ chart3.R
@@ -5,37 +5,20 @@ library(styler)
 library(dplyr)
 library(tidyr)
 
-# Which month(s) have the highest death rates?
+unemployed <-Unemployment_insurance
 
-race <- read_csv("C:/Users/lynnc/Downloads/CRDT Data - CRDT (1).csv")
+# Filter Unemployment insurance data to only get Non-seasonal adjustment and seasonal adjustment data
+NSA_and_SA<-Unemployment_insurance%>% 
+  select(
+  X,
+  X.1, 
+  X.3, 
+  X.5, 
+  X.7, 
+  X.11
+  )
 
-#deaths_data <- 
-  #select(race) %>%
-  #gather(key = State, value = Deaths_Total)
-
-deaths_data<- race %>% 
-    select (State, 
-    Deaths_Total,
-    Deaths_White, 
-    Deaths_Asian, 
-    Deaths_Latinx,
-    Deaths_Black, 
-    Deaths_Multiracial, 
-    Deaths_Other, 
-    Deaths_Ethnicity_Hispanic, 
-    Deaths_AIAN, 
-    Deaths_NHPI,
-    Deaths_Other,
-    Deaths_Unknown)%>%
-    #gather(key = Deaths_Total, value = State, na.rm = FALSE)
-    gather(key = State, value = Deaths_Total)%>%
-    mutate(State = State)
+View(NSA_and_SA)
 
 
-View(deaths_data)
-
-ggplot(deaths) + 
-  geom_col(
-    mapping = aes(x = State, y = Deaths_Total),
-    fill = deaths
-    )
+ggplot(NSA_and_SA)
