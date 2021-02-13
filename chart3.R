1970c71fd8051a67dd8ca84d74eddcc3e7c4f641
Lynn Chou <lynnchou8@gmail.com>
2021-02-12 19:04:18 -0800

    which month(s) have the highest death rates?

---
+++ chart3.R
@@ -0,0 +1,12 @@
+library(lintr)
+library(ggplot2)
+library(tidyverse)
+library(styler)
+
+# Which month(s) have the highest death rates?
+
+race <- Georgetown_by_race
+
+length(race$`Month of Month of Period`)
+
+
