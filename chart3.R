f99819c9e5592f1da05d7eaf27f8a75ffc76abe8
Lynn Chou <lynnchou8@gmail.com>
2021-02-13 12:41:19 -0800

    just making the dataframe

--- chart3.R
+++ chart3.R
@@ -5,8 +5,29 @@ library(styler)
 
 # Which month(s) have the highest death rates?
 
-race <- Georgetown_by_race
+race <- read_csv("C:/Users/lynnc/Downloads/CRDT Data - CRDT (1).csv")
 
-length(race$`Month of Month of Period`)
+deaths<- race %>%
+  select(Deaths_Total,
+         Deaths_White, 
+         Deaths_Asian, 
+         Deaths_Latinx,
+         Deaths_Black, 
+         Deaths_Multiracial, 
+         Deaths_Other, 
+         Deaths_Ethnicity_Hispanic, 
+         Deaths_AIAN, 
+         Deaths_NHPI,
+         Deaths_Other, 
+         Deaths_Unknown)
 
+states <- race%>%
+  pull(State)
+
+states_death <- merge(states, deaths)
+       
+       #(Deaths_White, Deaths_Black, Deaths_Latinx, Deaths_Asian)
+
+ggplot(states_death) + 
+  geom_bar(mapping = aes(x = race, y = deaths))
 
