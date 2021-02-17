12e9458aac49449a29ab96d1b39c3795757feb5f
Tammy Hu <tammyhu@uw.edu>
2021-02-17 15:10:15 -0800

    updated

--- chart1.R
+++ chart1.R
@@ -10,15 +10,15 @@ total_sample_size <- sum(unemployment$Sample.Size)
 
 
 # modified table for graphing
-mod_table <- unemployment %>% 
-  filter(ï..Labor.Force.Status == "Unemployed") %>% 
-  group_by(Race) %>% 
+mod_table <- unemployment %>%
+  filter(ï..Labor.Force.Status == "Unemployed") %>%
+  group_by(Race) %>%
   summarize(num_unemployed = sum(Sample.Size, na.rm = T) / 
               total_sample_size * 100)
 
 # graph comparing unemployment percentages by race
 unemployment_by_race <- ggplot(data = mod_table) +
-  geom_col(mapping = aes(x = Race, y = num_unemployed, color = Race, 
+  geom_col(mapping = aes(x = Race, y = num_unemployed, color = Race,
                          fill = Race), show.legend = F) +
   labs(x = "Race",
        y = "Percent Unemployed",
--- chart2.R
+++ chart2.R
@@ -9,14 +9,14 @@ unemployment <- read.csv("https://raw.githubusercontent.com/info201a-w21/covid-
 total_sample_size <- sum(unemployment$Sample.Size)
 
 # modified table for graphing employment by month
-new_table <- unemployment %>% 
-  group_by(Month.of.Month.of.Period) %>% 
-  filter(ï..Labor.Force.Status == "Employed - at work") %>% 
-  summarize(num_employed = sum(Sample.Size)) %>% 
+new_table <- unemployment %>%
+  group_by(Month.of.Month.of.Period) %>%
+  filter(ï..Labor.Force.Status == "Employed - at work") %>%
+  summarize(num_employed = sum(Sample.Size)) %>%
   arrange(match(Month.of.Month.of.Period, month.name))
 
 # graph of employed numbers by month
-employment_by_month <- ggplot(new_table, aes(month.abb[1:11], 
+employment_by_month <- ggplot(new_table, aes(month.abb[1:11],
                                              num_employed, group = 1)) +
   geom_point(size = 2, color = "#2ec4b6") +
   geom_line(size = 1, color = "#2ec4b6") +
