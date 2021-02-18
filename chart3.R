42525f9d24b413ee9be1bbdbf82da60e5aa92f0e
Lynn Chou <lynnchou8@gmail.com>
2021-02-18 10:25:08 -0800

    changes made

--- chart3.R
+++ chart3.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(tidyr)
 library(readr)
 
-employment<- read.csv("https://raw.githubusercontent.com/info201a-w21/covid-unemployment/main/OECD_Data_EA-19_and_G-7.csv")
+employment<- read.csv("OECD_Data_EA-19_and_G-7.csv")
 
 View(employment)
 
