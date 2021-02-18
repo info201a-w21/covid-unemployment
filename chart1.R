ca55828bad9701b58de21146e9eaee0ad6c44eb1
Lynn Chou <lynnchou8@gmail.com>
2021-02-18 11:41:48 -0800

    changes made to excel

--- chart1.R
+++ chart1.R
@@ -7,12 +7,12 @@ unemployment <- read.csv("Georgetown-by-race.csv") #[LC, prof mikes suggestion!]
 
 total_sample_size <- sum(unemployment$Sample.Size)
 
-# change column name from ï..Labor.Force.Status to Labor.Force.Status [LC]
-names(unemployment)[names(unemployment) == "ï..Labor.Force.Status"] <- "Labor.Force.Status" 
+# change column name from o..Labor.Force.Status to Labor.Force.Status [LC]
+#names(unemployment)[names(unemployment) == "o..Labor.Force.Status"] <- "Labor.Force.Status" 
 
 # modified table for graphing
 mod_table <- unemployment %>%
-  filter(Labor.Force.Status == "Unemployed") %>% #[LC]
+  filter(X...Labor.Force.Status. == "Unemployed") %>% #[LC]
   group_by(Race) %>%
   summarize(num_unemployed = sum(Sample.Size, na.rm = T) / 
               total_sample_size * 100)
