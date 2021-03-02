ad53ade113e8781cd0a26658ad79c32759b45de1
annashi127 <ashi127@uw.edu>
2021-03-01 18:42:25 -0800

    Updated table

--- table.R
+++ table.R
@@ -25,7 +25,10 @@ View(employment_rate_global)
  
 # Number of loss or gain jobs by race
 job_gains_or_loss_by_race <- covid_unemployment %>%
-  group_by(Race) %>%
+  group_by(Race, month) %>%
+  filter(month != "February", month != "April", month != "May", 
+         month != "June", month != "July", month != "August", 
+         month != "September", month != "October") %>%
   summarize(total_change = sum(Number)) # Biggest impact on White community
   
 # White with covid out of total white pop
@@ -35,7 +38,7 @@ white_covid_ratio <- 33146501 / 234370202 # 14% of white pop lost jobs
 Black_covid_ratio <- 9971075 / 44100000 # 22.6% of black pop lost jobs
 
 # Hispanics with covid out of total Hispanics pop
-Hispanics_covid_ratio <- 12195967 / 60600000 # 20 of hispanic pop lost jobs
+Hispanics_covid_ratio <- 12195967 / 60600000 # 20% of hispanic pop lost jobs
 
 
 
