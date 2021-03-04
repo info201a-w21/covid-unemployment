2306a16539e707ade892d3cd64df9d4705a3cb2f
Tammy Hu <tammyhu@uw.edu>
2021-03-04 14:45:50 -0800

    Shiny app stuff

---
+++ finalproject/app_server.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+library(plotly)
+library(tidyverse)
+
+server <- function(input, output) {
+
+}
---
+++ finalproject/app_ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(tidyverse)
+
+
+ui <- navbarPage(
+  "Final Project"
+)
