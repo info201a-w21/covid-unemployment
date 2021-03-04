5896c37f9c59e00491bd0f3b86801104bffe51d9
Tammy Hu <tammyhu@uw.edu>
2021-03-04 14:52:39 -0800

    Shiny app stuff

--- finalproject/app_ui.R
+++ finalproject/app_ui.R
@@ -5,5 +5,31 @@ library(tidyverse)
 
 
 ui <- navbarPage(
-  "Final Project"
+  "Final Project",
+  page_one,
+  page_two,
+  page_three,
+  page_four,
+  page_five
+)
+
+page_one <- tabPanel(
+  "Introduction", # label for the tab in the navbar
+  titlePanel("Welcome!"), # show with a displayed title
+)
+
+page_two <- tabPanel(
+  "Interactive Viz 1", # label for the tab in the navbar
+)
+
+page_three <- tabPanel(
+  "Interactive Viz 2", # label for the tab in the navbar
+)
+
+page_four <- tabPanel(
+  "Interactive Viz 3", # label for the tab in the navbar
+)
+
+page_five <- tabPanel(
+  "Summary", # label for the tab in the navbar
 )
