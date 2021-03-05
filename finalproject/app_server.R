ec973c69b6d95df7a9925df0d21bb2f01ef6c9cf
Tammy Hu <tammyhu@uw.edu>
2021-03-05 11:39:15 -0800

    added interactive map 1

--- finalproject/app_server.R
+++ finalproject/app_server.R
@@ -4,6 +4,75 @@ library(tidyverse)
 library(plotly)
 library(tidyverse)
 
-server <- function(input, output) {
+# Tammy's interactive stuff, chart set up:
+# unemployment graph by race, total (both female and male)
+unemploy <- read.csv("https://raw.githubusercontent.com/info201a-w21/covid-unemployment/main/unemploy_by_race_bureau.csv", header = T)
 
+# Renaming columns, preparing for chart
+unemploy <- rename(unemploy, race = ï..race)
+unemploy$year <- sapply(unemploy$year, toString)
+
+
+server <- function(input, output) {
+  # Tammy's interactive chart (1), do not change
+  output$unemployment <- renderPlotly({
+    unemployment_chart <- ggplot(data = unemploy) +
+      geom_bar(mapping = aes_string(x = "race", y = input$y_var, fill = "year"),
+               position = "dodge", stat = "identity") +
+      scale_fill_brewer(palette = input$color) +
+      theme_minimal() +
+      labs(x = "Race", y = "Unemployment Rates") +
+      if (input$y_var == "total_16_over") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 16 and over"
+        )
+      } else if (input$y_var == "ages_16_to_19") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 16 to 19"
+        )
+      } else if (input$y_var == "ages_18_to_19") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 18 to 19"
+        )
+      } else if (input$y_var == "ages_20_over") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 20 and over"
+        )
+      } else if (input$y_var == "ages_20_to_24") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 20 to 24"
+        )
+      } else if (input$y_var == "ages_25_and_over") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 25 and over"
+        )
+      } else if (input$y_var == "ages_25_to_54") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 25 to 54"
+        )
+      } else if (input$y_var == "ages_35_to_44") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 35 to 44"
+        )
+      } else if (input$y_var == "ages_45_to_54") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 45 to 54"
+        )
+      } else if (input$y_var == "ages_55_and_over") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 55 and over"
+        )
+      } else if (input$y_var == "ages_55_to_64") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 55 to 64"
+        )
+      } else if (input$y_var == "ages_65_and_over") {
+        labs(
+          title = "Comparing Unemployment Rates across Races for ages 65 and over"
+        )
+      }
+    ggplotly(unemployment_chart)
+  })
+  
+  # Add your server stuff in here
 }
--- finalproject/app_ui.R
+++ finalproject/app_ui.R
@@ -20,6 +20,21 @@ page_one <- tabPanel(
 
 page_two <- tabPanel(
   "Interactive Viz 1", # label for the tab in the navbar
+  titlePanel("Unemployment by Race during Covid"),
+  sidebarPanel(
+    age_input,
+    color_input
+  ),
+  mainPanel(
+    plotlyOutput(outputId = "unemployment"),
+    h2("About this chart"),
+    p("This chart reveals trends between race groups in the U.S. based on
+      unemployment rate during the Covid-19 pandemic, as compared to the
+      previous year, 2019. The dropdown menu allows filtering between a
+      variety of age groups.", em("Note: (his_lat stands for Hispanic or
+      Latino ethnicity, which can be of any race)."), "This dataset comes 
+      from the", em("U.S. Bureau Labor of Statistics."))
+  )
 )
 
 page_three <- tabPanel(
@@ -32,4 +47,23 @@ page_four <- tabPanel(
 
 page_five <- tabPanel(
   "Summary", # label for the tab in the navbar
+)
+
+# Tammy's input widgets
+# select y variable (age groups)
+age_input <- selectInput(
+  inputId = "y_var",
+  label = "Choose an age group",
+  choices = c("total_16_over", "ages_16_to_19",  
+              "ages_18_to_19", "ages_20_over", "ages_20_to_24", 
+              "ages_25_and_over", "ages_25_to_54", 
+              "ages_35_to_44", "ages_45_to_54", "ages_55_and_over",
+              "ages_55_to_64", "ages_65_and_over")
+)
+
+# select color of unemployment chart
+color_input <- selectInput(
+  inputId = "color",
+  label = "Choose a color palette",
+  choices = c("Reds", "Oranges", "Greens", "Blues", "Purples", "Greys")
 )
