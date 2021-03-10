8cca3fdf7bb3c22bb4876c3a2acee99c0343021f
Lynn Chou <lynchou8@gmail.com>
2021-03-09 19:26:31 -0800

    Lynn inserted her stuff into finalproject

--- finalproject/app_ui.R
+++ finalproject/app_ui.R
@@ -10,7 +10,10 @@ ui <- navbarPage(
   page_two,
   page_three,
   page_four,
-  page_five
+  page_five,
+  "Employment data",
+  employment_total,
+  employment_visual
 )
 
 page_one <- tabPanel(
@@ -102,3 +105,82 @@ color_input <- selectInput(
   label = "Choose a color palette",
   choices = c("Reds", "Oranges", "Greens", "Blues", "Purples", "Greys")
 )
+
+# Second chart: Employment data
+choose_country <- unique(employment_merge$Country)
+
+input_country <- selectInput(
+  inputId = "name_of_country",
+  label = "Choose a country",
+  choices = c(choose_country),
+  selected = "Australia"
+)
+
+stamp_time <- checkboxGroupInput(
+  inputId = "pick_a_quarter",
+  label = "Choose a Quarter",
+  choices = c("2019-Q1" = "2019-Q1",
+              "2019-Q2" = "2019-Q2",
+              "2019-Q3" = "2019-Q3",
+              "2019-Q4" = "2019-Q4",
+              "2020-Q1" = "2020_Q1",
+              "2020-Q2" = "2020-Q2",
+              "2020-Q3" = "2020-Q3",
+              "2020-Q4" = "2020-Q4"),
+  selected = "2019-Q1"
+)
+
+employment_total <- tabPanel(
+  "Total working population employment data",
+  titlePanel("Bar chart that shows the percentage of total working age population (15-64)"),
+  
+  sidebarLayout(
+    sidebarPanel(input_country, stamp_time),
+    mainPanel(textOutput("new_text"),
+              plotlyOutput("new_chart"))
+  )
+)
+choose_country <- unique(employment_merge$Country)
+
+country_input <- selectInput(
+  inputId = "countryName",
+  label = "Choose a country",
+  choices = c(choose_country),
+  selected = "Australia"
+)
+
+time_stamp <- checkboxGroupInput(
+  inputId = "Timeline",
+  label = "Choose a Quarter",
+  choices = c("2019-Q1" = "2019-Q1",
+              "2019-Q2" = "2019-Q2",
+              "2019-Q3" = "2019-Q3",
+              "2019-Q4" = "2019-Q4",
+              "2020-Q1" = "2020_Q1",
+              "2020-Q2" = "2020-Q2",
+              "2020-Q3" = "2020-Q3",
+              "2020-Q4" = "2020-Q4"),
+  selected = "2019-Q1"
+)
+
+
+employment_visual <- tabPanel(
+  "Change in total working population employment data",
+  titlePanel("Bar chart that shows the change in percentage of total working age population (15-64)"),
+  
+  sidebarLayout(
+    sidebarPanel(country_input, time_stamp),
+    mainPanel(textOutput("display_text"),
+              plotlyOutput("bar_chart"))
+  ) 
+)
+
+
+
+
+
+
+
+
+
+
