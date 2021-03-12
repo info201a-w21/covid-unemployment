f347d8ede34842375c6807fe2bcf95278f8bd5fa
annashi127 <ashi127@uw.edu>
2021-03-11 18:03:58 -0800

    Added new image

--- finalproject/app.R
+++ finalproject/app.R
@@ -10,7 +10,7 @@ library(readr)
 
 # Source in `app_ui.R` and `app_server.R`
 source("app_ui.R")
-source("app_server.R")
+# source("app_server.R")
 
 # Run the application 
 shinyApp(ui = ui, server = server)
